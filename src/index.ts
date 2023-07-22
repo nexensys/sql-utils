d21189191c10a810bf6292122af3705f6291246b
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-07-22 14:59:30 -0600

    laiudbfjknla.z

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { TableColumn } from "./datatypes";
+import { ColumnType, TableColumn } from "./datatypes";
 import { Table } from "./schema/Table";
 
 // Minimal functionality, but shouldn't change much
-export { Table, TableColumn };
+export { Table, TableColumn, ColumnType };
